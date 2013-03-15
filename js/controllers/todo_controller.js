69f14d95128f1b7425960d6aae6a434d58bd40ea
Trek Glowacki <trek.glowacki@gmail.com>
2013-03-14 21:53:35 -0500

    Accepting Edits

--- js/controllers/todo_controller.js
+++ js/controllers/todo_controller.js
@@ -2,6 +2,20 @@ Todos.TodoController = Ember.ObjectController.extend({
   actions: {
     editTodo: function () {
       this.set('isEditing', true);
+    },
+    acceptChanges: function () {
+      this.set('isEditing', false);
+
+      if (Ember.isEmpty(this.get('model.title'))) {
+        this.send('removeTodo');
+      } else {
+        this.get('model').save();
+      }
+    },
+    removeTodo: function () {
+      var todo = this.get('model');
+      todo.deleteRecord();
+      todo.save();
     }
   },
 
